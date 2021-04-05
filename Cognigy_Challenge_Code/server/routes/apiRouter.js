a6413f9a71ecbeee28888e9454c761243cc71f9c
adelhanifa <adel.hanifa@hotmail.com>
2021-04-05 20:36:24 +0200

    delete

--- Cognigy_Challenge_Code/server/routes/apiRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/apiController");
-const validator = require("../middlewares/inputs_validator");
-
-router.post("/", validator, controller.getPasswords);
-
-module.exports = router;
